0cd2707435ab8bee8226098b239c129aaa2804fa
Suyatna <zuyatna@gmail.com>
2022-06-29 10:29:29 +0700

    adding images, scrollable, and implement ClipRRect

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,72 +15,6 @@ class MyApp extends StatelessWidget {
       title: 'Wisata Bandung',
       theme: ThemeData(),
       home: const DetailScreen(),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(top: 16.0),
-              child: const Text(
-                'Farm House Lembang',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ), // Title
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: const <Widget>[
-                      Icon(Icons.calendar_today),
-                      SizedBox(height: 8.0),
-                      Text('Open Everyday')
-                    ],
-                  ), // Calendar
-                  Column(
-                    children: const <Widget>[
-                      Icon(Icons.access_time_outlined),
-                      SizedBox(height: 8.0),
-                      Text('09:00 - 20.00')
-                    ],
-                  ), // Access_Time
-                  Column(
-                    children: const <Widget>[
-                      Icon(Icons.attach_money_rounded),
-                      SizedBox(height: 8.0),
-                      Text('RP.25000')
-                    ],
-                  ), // Money
-                ],
-              ),
-            ), // Icon_Desc
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: const Text(
-                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ), // Description
-          ],
-        ),
-      ),
     );
   }
 }
