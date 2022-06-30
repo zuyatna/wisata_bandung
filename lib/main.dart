47c779b479eeababe732a56fc2e9bc58c5023af6
Suyatna <zuyatna@gmail.com>
2022-06-30 19:21:14 +0700

    can tap on card and go detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'detail_screen.dart';
 import 'main_screen.dart';
 
 void main() {
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'detail_screen.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -9,30 +10,37 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Wisata Bandung'),
       ),
-      body: Card(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(child: Image.asset('images/farm-house.jpg'),),
-            Expanded(
-              flex: 2,
-              child: Padding(
-              padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: const <Widget>[
-                    Text(
-                      'Farm House Lembang',
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                    SizedBox(height: 10,),
-                    Text('Lembang')
-                  ],
+      body: InkWell(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const DetailScreen();
+          }));
+        },
+        child: Card(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(child: Image.asset('images/farm-house.jpg'),),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: const <Widget>[
+                      Text(
+                        'Farm House Lembang',
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                      SizedBox(height: 10,),
+                      Text('Lembang')
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
